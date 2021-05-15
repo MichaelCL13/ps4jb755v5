797c70894beeba69006a30bd8d8cb161df475f6c
MichaelCL13 <michaelcuellarleon@gmail.com>
2021-05-15 20:49:19 +0200

    755 v5

---
+++ changelog.js
@@ -0,0 +1,44 @@
+let changes=`2021-05-05:
+  - Added WebRTE payload.
+  - Added GoldHEN version option to reduce clutter.
+2021-05-04:
+  - Implemented multi-payload launching.
+  - Implemented custom payload chains.
+  - Added latest kernel exploit (beware, it might be unstable).
+2021-04-30:
+  - Merged 7.02 and 7.5x HTML - please report any bugs on GitHub.
+  - Added option to switch between Kernel exploits.
+  - Updated Linux payloads.
+2021-04-29:
+  - Fixed log level option.
+  - Fixed 7.5x-specific layout bug.
+  - Implemented payload styles.
+  - Implemented color schemes.
+  - Added option to switch between WebKit exploits.
+  - Wallpaper caching now displays progress.
+2021-04-22: Updated HEN 2.1.3b (fixes freezing upon closing games).
+2021-04-21:
+  - Added HEN 2.1.3b.
+  - Reworked jailbreak history logging.
+2021-04-14: Implemented wallpapers and wallpaper cache.
+2021-04-13: Added Web Activator.
+2021-04-09:
+  - Added GoldHEN 1.1.
+  - Fixed not loading jailbreak on first try.
+2021-04-07:
+  - Improved logging.
+  - Added average attempts and time calculation.
+2021-04-04:
+  - Implemented Jailbreak history and Settings.
+  - Firmware check now ignores spoof.
+  - Added sleirsgoevy's Linux payloads.
+  - Updated Nazky's Linux payloads.
+  - Updated Mira and jb.js with sleirsgoevy's latest changes.
+2021-03-29:
+  - Fixed not going back to main page after successful jailbreak.
+  - The WebKit exploit now resumes after error "Failed to setup a relative read primitive".
+2021-03-28: Added Linux payloads.
+2021-03-27: Added firmware safety checks.`;
+let date="2021-05-05";
+let build="1";
+let targetFirmware="7.5x";
